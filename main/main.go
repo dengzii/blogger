4c169802b2839db9d44e1ff0669a77dcdf03ff19
dengzii <dengzii@foxmail.com>
2021-01-02 22:26:42 +0800

    update action

--- main/main.go
+++ main/main.go
@@ -8,10 +8,10 @@ import (
 
 func main() {
 
-	wh := webhook.New("0.0.0.0", "actions/", 8080)
+	wh := webhook.New("0.0.0.0", "/actions/", 8080)
 
 	wh.Register("1", "abcd", func(id string, params url.Values) {
-		fmt.Println("Do Register " + id)
+		fmt.Println("action triggered")
 	})
 
 	wh.Listen()
---
+++ webhook/log.go
@@ -0,0 +1,7 @@
+package webhook
+
+import "fmt"
+
+func logDebug(log string) {
+	fmt.Printf("%s\n", log)
+}
