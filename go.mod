23534249f3963a36e8815512d70f93e14917a155
dengzii <dengzii@foxmail.com>
2021-01-03 22:21:39 +0800

    repo

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/BurntSushi/toml v0.3.1
-	github.com/russross/blackfriday v1.6.0
+	github.com/go-git/go-billy/v5 v5.0.0
+	github.com/go-git/go-git/v5 v5.2.0
 )
--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	Blogger "blogger"
 	"blogger/webhook"
 	"fmt"
 	"net/url"
@@ -8,6 +9,12 @@ import (
 
 func main() {
 
+	r := Blogger.GitRepo{
+		Url:        "https://github.com/dengzii/RespberryPi",
+		StorageDir: "./repo",
+	}
+	r.Remove()
+	//r.Update()
 	wh := webhook.New("0.0.0.0", "/actions/", 8080)
 
 	wh.Register("1", "abcd", func(id string, params url.Values) {
